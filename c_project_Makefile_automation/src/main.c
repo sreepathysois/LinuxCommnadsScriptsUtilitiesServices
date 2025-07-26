260a4df19b8554212d2fcd3a49ae90f3773e4a08
sreepathysois <54054574+sreepathysois@users.noreply.github.com>
2025-07-26 14:23:32 +0530

    Create main.c

---
+++ c_project_Makefile_automation/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math.h"
+#include "io.h"
+
+int main() {
+    int a = 10, b = 5;
+    printf("Sum: %d\n", add(a, b));
+    log_message("Main completed.");
+    return 0;
+}
